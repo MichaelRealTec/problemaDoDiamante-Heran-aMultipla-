5734b0562705240f9de6646101dbbb71ebe39ae4
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2023-12-23 00:12:18 -0300

    implementando a Classe Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,21 @@
 package application;
 
+import devices.Printer;
+import devices.Scanner;
+
 public class Program {
 	public static void main(String[] args) {
 		// Problema do diamente (Heranças Multiplas)
 		
+		// Criando um novo objeto impressora
+		Printer p = new Printer("1080");
+		p.processDoc("My Letter"); // My letter = Minha Carta
+		p.print("My Letter"); // My Letter = Minha Carta
 		
-		
+		// Criando um novo objeto Scanner
+		Scanner s = new Scanner("2003"); // Número de serial do Scanner
+		s.processDoc("My Email"); // My Email = meu email
+		System.out.println("Scan result: " + s.scan()); // Imprimir resultado
 		
 	}
 }
